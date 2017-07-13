681b7ff5dd856ed7aa0d6d7f1cdf55e094382ce8
Lama Beddawi <lbeddawi84@gmail.com>
2017-07-13 15:15:05 -0500

    added some code

--- LibraryCardCatalog/Program.cs
+++ LibraryCardCatalog/Program.cs
@@ -10,6 +10,30 @@ namespace LibraryCardCatalog
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Please enter a file name");
+            
+            string FileName = Console.ReadLine();
+
+
+            // make sure to add a defensive mechanism against the entry of the user
+            Console.WriteLine("Please choose an option");
+            Console.WriteLine("1. List all books");
+            Console.WriteLine("2. Add a book");
+            Console.WriteLine("3. Save and exit");
+            string Selection = Console.ReadLine();
+            
+            int Choice = Convert.ToInt32(Selection);
+
+            //bool result = Int32.TryParse(Selection, out Choice);
+            
+            
+
+            
+            {
+                
+            }
+
         }
     }
 }
